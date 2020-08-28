dd413cd0e5bb31fdbe7f04834bc6fc73c510ddd5
Luis Locon <luis.locon@gmail.com>
2020-08-27 20:01:45 -0600

    [First example a counter]

---
+++ src/examples/Counter.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Counter: React.FC = () => {
+  const [counter, setCounter] = React.useState<number>(0);
+
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  const handleDecrement = () => {
+    !counter ? handleReset() : setCounter(counter - 1);
+  };
+  const handleReset = () => {
+    setCounter(0);
+  };
+  return (
+    <div>
+      <p>The current count is {counter}</p>
+      <button onClick={handleDecrement}>- 1</button>
+      <button onClick={handleIncrement}>+ 1</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import App from './App';
+import * as serviceWorker from "./serviceWorker";
+import Counter from "./examples/Counter";
+
+const App: React.FC = () => (
+  <div>
+    <h1>Show Examples</h1>
+    <Counter />
+  </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
